3c281f4a5e06368946db741ed1bd9aa1ccff491d
Caroline <102994565+bycaroline@users.noreply.github.com>
2024-12-19 16:01:11 +0100

    Update Paragraph2.js

--- src/components/Paragraph2.js
+++ src/components/Paragraph2.js
@@ -9,7 +9,7 @@ function Paragraph2() {
                 <p>I am currently enrolled in a two-year Java program at Campus Mölndal in Gothenburg. My portfolio showcases some of the projects I have worked on during my Java studies, as well as projects from my previous studies in frontend development.</p>
             </div>
             <div>
-                <p>One project is a webshop that I developed collaboratively with other students. During this project, we implemented the Scrum methodology and utilized version control. Another project featured in my portfolio is an updated version of this portfolio, that I am working on. It consists of a Java server in the backend and a React client in the frontend.</p>
+                <p>My Exam project aims to explore how machine learning works while applying Python skills and integrating them with Java and React. The outcome is a service that predicts and estimates air quality in specific areas of Stockholm's inner city. Another project is a webshop that I developed collaboratively with other students. During this project, we implemented the Scrum methodology and utilized version control. </p>
 
             </div>
         </div>
